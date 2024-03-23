3506cd26d7e253640adaa1bb2d93590ea7733bf7
Josh Garverick <josh.garverick@gmail.com>
2024-03-22 21:55:23 -0400

    Update install.sh

--- src/pre-commit/install.sh
+++ src/pre-commit/install.sh
@@ -4,8 +4,8 @@ if command -v pip >/dev/null 2>&1; then
 echo 'System has pip installed.'
 else
   echo '[INFO] Installing pip...'
-  sudo dnf updateinfo
-  sudo dnf install -y python3*-pip
+  dnf updateinfo
+  dnf install -y python3*-pip
 fi
 
 pip install pre-commit
