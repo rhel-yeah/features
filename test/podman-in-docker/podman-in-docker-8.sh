5407c1452a332c5c1aa633935931c459c7895031
Josh Garverick <josh.garverick@gmail.com>
2024-06-15 15:40:15 +0000

    Removing the alias nonsense since it doesn't seem to work

--- test/podman-in-docker/podman-in-docker-8.sh
+++ test/podman-in-docker/podman-in-docker-8.sh
@@ -4,7 +4,6 @@ source dev-container-features-test-lib
 
 check "RHEL Version" cat /etc/redhat-release | grep "release 8"
 
-alias docker=podman
 podman info
-docker --help
-docker pull docker.io/library/redis:latest
+podman --help
+podman pull docker.io/library/redis:latest
