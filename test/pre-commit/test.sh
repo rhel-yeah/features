0068363f7bf4ac1693d40c405820a901c3b90f49
Josh Garverick <josh.garverick@gmail.com>
2024-06-15 00:33:29 +0000

    Trying another angle

--- test/pre-commit/test.sh
+++ test/pre-commit/test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+yum install -y git
+git init test-repo --initial-branch main
+cp .pre-commit-config.yaml test-repo/
+cd test-repo
+pre-commit install
+pre-commit install hooks
 pre-commit run -a
