b4de79175dc51b5151eb0a0de99223b6698a03e7
Josh Garverick <josh.garverick@gmail.com>
2024-06-15 03:23:23 +0000

    Testing out RHEL versions

--- src/podman-in-docker/install.sh
+++ src/podman-in-docker/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
 yum update
-yum module enable -y container-tools 
-yum module install -y container-tools
+# Evaluate if the contents of /etc/redhat-release contain release 8
+# If it does, then the script will install the podman-docker package
+# If it does not, then the script will install the podman package
+if [[ $(cat /etc/redhat-release) == *"release 8"* ]]; then
+    yum module enable -y container-tools:rhel8 
+    yum module install -y container-tools:rhel8
+elif [[ $(cat /etc/redhat-release) == *"release 7"* ]]; then
+    subscription-manager repos --enable=rhel-7-server-extras-rpms
+    yum -y install podman
+elif [[ $(cat /etc/redhat-release) == *"release 9"* ]]; then
+    echo "Coming soon"
+else
+    echo "RHEL version found is not supported"
+    exit 1
+fi
+
+
